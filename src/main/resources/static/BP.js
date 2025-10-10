e727f22e461a5e5ac2a8e1b1d438fbad3c1fa6c6
Guilherme Ribeiro <ggr12gui@hotmail.com>
2025-10-10 18:46:53 -0300

    Correção 1

--- src/main/resources/static/BP.js
+++ src/main/resources/static/BP.js
@@ -1,5 +1,20 @@
 
 
 function cadastro(){
-       alert('Produto cadastrado com sucesso!');
+    
+    const txtProduto = document.getElementById("txtProduto").value
+    const txtMarca = document.getElementById("txtMarca").value
+    const txtPreco = document.getElementById("txtPreco").value
+    const txtQuantidade = document.getElementById("txtQuantidade").value
+    const txtData = document.getElementById("txtData").value   
+        
+    if (txtProduto == "" || txtMarca == "" || txtPreco == ""|| txtQuantidade == "" || txtData == ""){
+        
+        alert('Preencha os campos corretamente!');
+        
+    } else {
+            
+        alert('Produto cadastrado com sucesso!');
+       
+           }
     }
